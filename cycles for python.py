3fd355a4782e73db86859232ab07620910a90183
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-01-12 21:58:02 +0300

    Add files via upload

---
+++ cycles for python.py
@@ -0,0 +1,17 @@
+numbers= [2,434,545,54]
+count=0
+for i in numbers:
+    print(i)
+    count+=1
+    print(count,'обход')
+    input()#it makes pause
+amount_of_numbers2= [24,43,43,30,300]
+for a in amount_of_numbers2:
+    a+=5#for each number we add 5
+    print (a)
+print(a)
+amount_of_numbers3=[24,434,42,322]
+for b in numbers3:
+    b-=10#for each number we calculate 10
+    print(b)
+print(b)
