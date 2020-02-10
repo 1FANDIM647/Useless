d3a60ca1b03736373598e4aac22ad72eb92f7d7e
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-02-10 20:02:35 +0300

    Add files via upload

---
+++ money_in_bank.py
@@ -0,0 +1,18 @@
+
+size_of_dollar =int(input("const in dollars:"))
+size_of_funts =int(input("Money in funts:"))
+
+dollar=int(input("Money in dollars:"))
+funt=int(input("Money in funts:"))
+
+rubles=dollar*size_of_dollar+size_of_funts*funt 
+
+if rubles==2100:
+     print("You are ok:",rubles)
+else:
+         print("You have:",rubles)
+   
+
+
+
+
