e71e40a0c6e9320ac210455914c852de1a143f13
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-02-11 19:51:01 +0300

    Add files via upload

---
+++ HERO_AND_DAMAGE.py
@@ -0,0 +1,9 @@
+print ("hero makes damage to enemy ")
+health=100
+trolls = 0
+damage = 10
+while health != 0  : 
+    trolls += 1
+    health -=damage
+    print ("Oh shit you have got ",health)
+print(" YOU ARE DEAD")
