b40c68f871b4b4a85246b0760862dbbc5f4c2434
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-02-18 17:32:10 +0300

    Add files via upload

---
+++ Far_net.py
@@ -0,0 +1,58 @@
+import random
+
+print("\t\t\t***Exclisve net of France***")
+print("\t***start process of autithenication***")
+print("*")
+print("*")
+print("*")
+print("*")
+print("*")
+print("*")
+#start variables
+security = 0
+username = ""
+while not username:
+    username = input("Login:")
+password= ""
+while not password:
+    password = input("Password:")
+if username  == "Mr.Elkin" and password == "elkin474290":
+    print("autithenication is over ")
+    print("welcome Mr.Elkin")
+    print("All scripts is loaded")
+    id=int(input("Enter id:"))
+    #in case if we wrote 12345
+    if(id == 12345):
+        number=0
+        while(number<5):
+            number +=1
+            print("\t***loading id ***")
+    #in case if we wrote 456        
+    elif(id == 456):
+        def loading ():
+            number=0
+            while(number<12):
+                number +=1
+                print("\t***loading id ***")
+    loading()             
+    print("all functions is loaded in DISK 'home" )             
+    new_key = random.randint(1,1000000000)
+    print ("Your key is :",new_key)
+    print("Use it here www.google.com ")
+    name_of_rep=input("enter name of your repository :")
+    print("****")
+    print("File  copied in  rep :",name_of_rep)
+    a = ""
+    print("continue:")
+    if a == "yes":
+        print("****")
+        print( 
+        "Loading",
+        "connecting with server",
+        "HTTP :133113.132314.4124.12.12")
+    else:
+        print("all functions were correct. Good luck ")
+
+else:
+    print("User is not  exisst ") 
+input("\n\nНажмите enter , чтобы выйти")      
