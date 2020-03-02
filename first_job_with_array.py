100ab91c47a03d78339574617d29f25830003877
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-03-02 19:53:27 +0300

    Add files via upload

---
+++ first_job_with_array.py
@@ -0,0 +1,7 @@
+array_int = [3,6,9,12,18]
+
+for num in array_int:
+    resultNumber = num /3 
+    print("result of " , num , "/3" , "result :" , resultNumber)
+    #loop will be over when will be over  the array
+    
