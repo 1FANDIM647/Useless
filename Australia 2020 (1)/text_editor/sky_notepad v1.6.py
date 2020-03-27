6d849986231604cc321df963fc3567409b234ebf
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-03-27 16:56:05 +0300

    Add files via upload

--- Australia 2020 (1)/text_editor/sky_notepad v1.6.py
+++ Australia 2020 (1)/text_editor/sky_notepad v1.6.py
@@ -5,7 +5,7 @@ from tkinter.messagebox import showerror
 from tkinter import messagebox
 from tkinter import *
 import webbrowser
-
+import tkinter as tk
 
 
 
@@ -57,8 +57,27 @@ def open_file():
 	data = inp.read()
 	text.delete('1.0', tkinter.END)
 	text.insert('1.0', data)
+#fonts of texts 
+def fonts():
+    root=tk.Tk()
+    var1=tk.IntVar()
+    var2=tk.IntVar()
+    check1=tk.Checkbutton(root,text=u'Русский times 10', font="times 10", anchor=tk.W, variable=var1,onvalue=1,offvalue=0)
+    check2=tk.Checkbutton(root,text=u'Русский times 13', font="times 13", anchor=tk.W, variable=var2,onvalue=1,offvalue=0)
+    check3=tk.Checkbutton(root,text=u'Русский clearlyu 13', font="clearlyu 13", anchor=tk.W, variable=var2,onvalue=1,offvalue=0)
+    check4=tk.Checkbutton(root,text=u'Русский clearlyu 15', font="clearlyu 15", anchor=tk.W, variable=var2,onvalue=1,offvalue=0)
+    check5=tk.Checkbutton(root,text=u'Русский terminal 13', font="terminal 13", anchor=tk.W, variable=var2,onvalue=1,offvalue=0)
+    check6=tk.Checkbutton(root,text=u'Русский terminal 15', font="terminal 15", anchor=tk.W, variable=var2,onvalue=1,offvalue=0)
+  
+    check1.pack(fill=tk.X, expand=True)
+    check2.pack(fill=tk.X, expand=True)
+    check3.pack(fill=tk.X, expand=True)
+    check4.pack(fill=tk.X, expand=True)
+    check5.pack(fill=tk.X, expand=True)
+    check6.pack(fill=tk.X, expand=True)
 
 
+    	
     	
    
  
@@ -122,7 +141,7 @@ menuBar.add_cascade(label="Info", command=info)
 menuBar.add_cascade(label="Exit", command=root.quit)
 menuBar.add_cascade(label="FUCK IT", command=root.quit)
 menuBar.add_cascade(label="Google" ,command=callback)
-
+menuBar.add_cascade(label="Fonts",command =fonts  )
 
 root.config(menu=menuBar)
 root.mainloop()
