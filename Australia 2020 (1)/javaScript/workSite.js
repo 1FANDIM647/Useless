00c4405706a0d8e25ec54cdf7d13838e7caa858c
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-03-29 19:07:40 +0300

    Add files via upload

---
+++ Australia 2020 (1)/javaScript/workSite.js
@@ -0,0 +1,16 @@
+// Input of some text  tp html site 
+
+function makeHeader (t)
+{
+    document.write("<h4>"+t+"</h4");
+    document.write("it is working");
+}
+function loop (f)
+{
+    for (var k=1; k <= f ; k++)
+    {  
+        // create variable
+        var txt = makeHeader ();
+    }
+}
+
