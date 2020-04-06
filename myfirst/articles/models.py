5f4a8ea8c5f3c59285d2513cc137bd1c5f96904a
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-04-06 20:32:35 +0300

    Add files via upload

--- myfirst/articles/models.py
+++ myfirst/articles/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 
-class  Article(models.Model):
 
-	article_title = models.CharField('Name of article' , max_length = 100)
-    article_text = models.TextField( 'Your comment' , max_length= 450)
-    pub_date = models.DateTimeField('data of publication')
+class Article(models.Model):
+	article_title = models.CharField('Name of article',max_length= 100)
+	article_text =models.TextField('Your comment',max_length=450)
+	pub_date = models.DateTimeField('data')
+
+
 
 	
 		
-class Comment(modesls.Model):
+class Comment(models.Model):
+	article = modesls.Foreignkey(Article, on_delete= models.CASCADE)
+	author_name= models.CharField('Name' , max_length = 49)
+	comment_text = models.CharField('comment' , max_length=450)
+
 	
-	article = modesls.Foreignkey(Article , on_delete = modesls.CASCADE)  
-    author_name = modesls.CharField('Name' , max_length = 51)
-    comment_text = modesls.CharField('comment' , max_length= 456)
 
     
  
