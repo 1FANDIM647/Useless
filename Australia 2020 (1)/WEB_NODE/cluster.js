5064fc6861052e551eb152e0e9f5155e491ab941
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-04-15 18:27:47 +0300

    Add files via upload

--- Australia 2020 (1)/WEB_NODE/cluster.js
+++ Australia 2020 (1)/WEB_NODE/cluster.js
@@ -2,11 +2,25 @@ const cluster = require (id: 'cluster');
 const os =require (id 'os');
 const pid = process.pid;
 
+// worker is our server
+
 if (cluster.isMaster){
   const cpusCount = os.cpus().length;
   console.log('CPUs: ${cpusCount}');
   console.log('Master started. Pid: ${pid}');
-  console.fork();
+  for (let i = 0 ; i <cpusCount-1; i++){
+     const worker = console.fork();
+     // take more cores but  we put last one
+     worker.on(event: 'exit' , listener:() => {
+         console.log ('Worker is died ${worker.process.pid}');
+         cluster.fork();
+
+     }); 
+
+     worker.send( message: 'Server has started  to work');
+
+  }
+  
 }
 if (cluster.isWorker)
 {
