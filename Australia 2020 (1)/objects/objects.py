e18172170eba9cde890c11a528d927670a6d83ea
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-04-18 20:30:26 +0300

    Add files via upload

---
+++ Australia 2020 (1)/objects/objects.py
@@ -0,0 +1,9 @@
+class Animal(object):
+	def talk():
+		print ("Hi")
+
+cat = Animal()
+cat.talk()
+
+input("\n\n Наимите Enter , чтобы выйти")		
+
---
+++ Australia 2020 (1)/objects/objects_with_constructor.py
@@ -0,0 +1,13 @@
+# this code descriebs how  to create  class and 
+# obkect this class
+
+class Animal (object ):
+	def __init__(self): #constructor
+		print("Hi I am ogject ")
+	def talk():
+		print("I can talk to you ")
+
+cat = Animal()
+cat.talk()
+
+input ("Press Enter")		
