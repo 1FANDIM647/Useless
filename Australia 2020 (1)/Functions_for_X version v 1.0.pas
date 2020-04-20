bee5493a93c4e817324523b084619fc1f3be32f4
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-04-20 16:45:04 +0300

    Add files via upload

---
+++ Australia 2020 (1)/Functions_for_X version v 1.0.pas
@@ -0,0 +1,25 @@
+﻿Program Function_for_x ;
+var x, k : integer ;
+
+var s :string;
+         
+begin 
+  
+  k:=0;
+ 
+    writeln('Введите x');
+    readln(x);
+   while x <> 0 do
+   begin
+     writeln('Введите x');
+     readln(x);
+     
+     if x > 1 then
+     begin
+       k:= k +1;
+     end;
+   end;
+  
+   writeln(k);
+   
+end. 
---
+++ Australia 2020 (1)/Functions_for_X.pas
@@ -0,0 +1,54 @@
+﻿// Программа которая считает
+// разные значения при  переменной x
+Program Function_for_x;
+
+var x ,k  : integer;
+var x1 ,k1: real;
+var s :string; 
+ 
+begin
+  
+
+writeln('Введите тип перемменной:');
+readln(s);
+if s = 'int' then
+// если  тип int  
+begin
+  writeln('Введите  x :');
+  readln(x);
+  if x > 1 then
+      begin
+        k:= k + 1;
+      end
+  else
+      begin
+     
+       if x = 0 then
+           begin
+               writeln('Если постоянно возращаться к вводу x , то это будет бесконечный цикл');  
+           end;
+      end;
+   writeln(k);
+  
+end;
+
+if s = 'double' then
+begin
+writeln('Введите  x :');
+  readln(x1);
+  if x1 > 1 then
+      begin
+        k1:= k1 + 1;
+      end
+  else
+      begin
+     
+       if x1 = 0 then
+           begin
+               writeln('Если постоянно возращаться к вводу x , то это будет бесконечный цикл');  
+           end;
+      end;
+   writeln(k1);
+end;
+ 
+end.
