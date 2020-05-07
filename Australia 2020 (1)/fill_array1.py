a4d8ccd1e6a741de0978289334f59f027e034ea6
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-05-07 19:30:53 +0300

    Add files via upload

---
+++ Australia 2020 (1)/fill_array1.py
@@ -0,0 +1,18 @@
+import random
+
+a = random.randint(0,9)
+
+arr = []
+
+enter_c = ""
+enter_c = input("enter:")
+
+while (enter_c != "end"):
+    arr.append(a)
+    enter_c = input("enter_c")
+
+
+   
+ 
+print (arr) 
+print ("it is over ")
