f8cc39c299acfbf21c3b055dbfdd930f5e9f3d6f
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-05-11 11:20:24 +0300

    Add files via upload

---
+++ Australia 2020 (1)/Scripts/all_points_in_array.py
@@ -0,0 +1,13 @@
+#we count all player's points
+
+def sum_points(arr):
+	total = 0
+	for x in arr:
+		total +=x
+	return total
+	
+
+result_all =sum_points([1,23,34,34,54,65,32,13,])
+print (result_all)
+		
+		
